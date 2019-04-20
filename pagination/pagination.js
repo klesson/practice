f0f5677c04512b188116a8c2a378ecd3633ce1aa
klesson <49154904+klesson@users.noreply.github.com>
2019-04-20 18:20:58 +0300

    Add files via upload

--- pagination/pagination.js
+++ pagination/pagination.js
@@ -2,14 +2,39 @@
 	window.onload = function() {			
 		var buttons = document.getElementsByClassName('button');
 		var curSelected = buttons.item(3);
-		for(var i = 0; i < buttons.length; i++) {
-			var btn = buttons.item(i);
-			btn.addEventListener('click', function(event) {
+		var btnCollection = [];
+		function selItem(target) {
 				console.log('click', event, event.target);
 				curSelected.classList.remove('selected');
-				event.target.classList.add('selected');
-				curSelected = event.target;
-			});
+				target.classList.add('selected');
+				curSelected = target;
 		}
+		buttons.item(0).addEventListener('click', function(event) {
+			var curIndex = btnCollection.indexOf(curSelected);
+			if (curIndex) {
+			curIndex--;
+			selItem(btnCollection[curIndex]);
+		}
+		});
+		for(var i = 1; i < buttons.length-2; i++) {
+			var btn = buttons.item(i);
+			btn.addEventListener('click', function(event) {
+				selItem(event.target);
+			});
+			btnCollection.push(btn);
+		}
+
+		buttons.item(buttons.length-2).addEventListener('click', function(event) {
+			var curIndex = btnCollection.indexOf(curSelected);
+			if ((curIndex > -1) && (curIndex<btnCollection.length - 1)) {
+			curIndex++;
+			selItem(btnCollection[curIndex]);
+		}
+		});
+
+		buttons.item(buttons.length-1).addEventListener('click', function(event) {
+			var curIndex = btnCollection.length - 1;
+			selItem(btnCollection[curIndex]);
+		});
 	}
 })();
